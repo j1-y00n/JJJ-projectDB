ecc032eacaa5b7889d9f07e138b5c10c25505c7a
j1-y00n <byunjiyun@naver.com>
2024-10-10 20:26:22 +0900

    port 수정정

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5005;
 
 server.use(cors());
 server.use(middlewares);
